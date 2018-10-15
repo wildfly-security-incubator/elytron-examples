7864bde956634488eb7820274e4e16e695cea693
Darran Lofthouse <darran.lofthouse@jboss.com>
2018-10-15 16:27:19 +0100

    Example illustrating using the WildFly Elytron JASPI implementation with stand alone Undertow.

---
+++ undertow-standalone-jaspi/src/main/java/org/wildfly/security/examples/HelloWorld.java
@@ -0,0 +1,141 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2017 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wildfly.security.examples;
+
+import static org.wildfly.security.password.interfaces.ClearPassword.ALGORITHM_CLEAR;
+
+import java.security.Provider;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.security.auth.message.config.AuthConfigFactory;
+
+import org.wildfly.elytron.web.undertow.server.servlet.AuthenticationManager;
+import org.wildfly.security.WildFlyElytronProvider;
+import org.wildfly.security.auth.jaspi.ElytronAuthConfigFactory;
+import org.wildfly.security.auth.jaspi.JaspiConfigurationBuilder;
+import org.wildfly.security.auth.permission.LoginPermission;
+import org.wildfly.security.auth.realm.SimpleMapBackedSecurityRealm;
+import org.wildfly.security.auth.realm.SimpleRealmEntry;
+import org.wildfly.security.auth.server.SecurityDomain;
+import org.wildfly.security.authz.RoleMapper;
+import org.wildfly.security.authz.Roles;
+import org.wildfly.security.credential.PasswordCredential;
+import org.wildfly.security.examples.jaspi.SimpleServerAuthModule;
+import org.wildfly.security.examples.servlet.SecuredServlet;
+import org.wildfly.security.password.PasswordFactory;
+import org.wildfly.security.password.spec.ClearPasswordSpec;
+import org.wildfly.security.permission.PermissionVerifier;
+
+import io.undertow.Handlers;
+import io.undertow.Undertow;
+import io.undertow.server.handlers.PathHandler;
+import io.undertow.servlet.Servlets;
+import io.undertow.servlet.api.DeploymentInfo;
+import io.undertow.servlet.api.DeploymentManager;
+import io.undertow.servlet.api.SecurityConstraint;
+import io.undertow.servlet.api.SecurityInfo;
+import io.undertow.servlet.api.WebResourceCollection;
+
+/**
+ * A simple project demonstrating how the WildFly Elytron JASPI integration can be programatically configured and used for stand
+ * alone integration with Undertow.
+ *
+ * @author <a href="mailto:darran.lofthouse@jboss.com">Darran Lofthouse</a>
+ */
+public class HelloWorld {
+
+    private static final WildFlyElytronProvider elytronProvider = new WildFlyElytronProvider();
+
+    private static final String HOST = "localhost";
+    private static final int PORT = 28080;
+    private static final String PATH = "/helloworld";
+    private static final String SERVLET = "/secured";
+
+    public static void main(String[] args) throws Exception {
+        SecurityDomain securityDomain = createSecurityDomain();
+
+        configureJaspic(securityDomain);
+
+        DeploymentInfo deploymentInfo = Servlets.deployment()
+                .setClassLoader(SecuredServlet.class.getClassLoader())
+                .setContextPath(PATH)
+                .setDeploymentName("helloworld.war")
+                .addSecurityConstraint(new SecurityConstraint()
+                        .addWebResourceCollection(new WebResourceCollection()
+                                .addUrlPattern("/secured/*"))
+                        .addRoleAllowed("Users")
+                        .setEmptyRoleSemantic(SecurityInfo.EmptyRoleSemantic.DENY))
+                .addServlets(Servlets.servlet(SecuredServlet.class)
+                        .addMapping(SERVLET));
+
+        AuthenticationManager authManager = AuthenticationManager.builder()
+                .setSecurityDomain(securityDomain)
+                .build();
+        authManager.configure(deploymentInfo);
+
+        DeploymentManager deployManager = Servlets.defaultContainer().addDeployment(deploymentInfo);
+        deployManager.deploy();
+
+        PathHandler path = Handlers.path(Handlers.redirect(PATH))
+                .addPrefixPath(PATH, deployManager.start());
+
+        Undertow server = Undertow.builder()
+                .addHttpListener(PORT, HOST)
+                .setHandler(path)
+                .build();
+        server.start();
+
+        System.out.println();
+        System.out.println(String.format("Ready for requests to http://%s:%d%s%s", HOST, PORT, PATH, SERVLET));
+        System.out.println();
+        System.out.println(String.format("e.g. 'curl http://%s:%d%s%s' \n", HOST, PORT, PATH, SERVLET));
+    }
+
+    private static String configureJaspic(final SecurityDomain securityDomain) {
+        AuthConfigFactory authConfigFactory = new ElytronAuthConfigFactory();
+        AuthConfigFactory.setFactory(authConfigFactory);
+
+        return JaspiConfigurationBuilder.builder(null, null)
+                .setDescription("Default Catch All Configuration")
+                .addAuthModuleFactory(SimpleServerAuthModule::new)
+                .register(authConfigFactory);
+    }
+
+    private static SecurityDomain createSecurityDomain() throws Exception {
+        PasswordFactory passwordFactory = PasswordFactory.getInstance(ALGORITHM_CLEAR, elytronProvider);
+
+        Map<String, SimpleRealmEntry> identityMap = new HashMap<>();
+        identityMap.put("elytron", new SimpleRealmEntry(Collections.singletonList(new PasswordCredential(passwordFactory.generatePassword(new ClearPasswordSpec("Coleoptera".toCharArray()))))));
+
+        SimpleMapBackedSecurityRealm simpleRealm = new SimpleMapBackedSecurityRealm(() -> new Provider[] { elytronProvider });
+        simpleRealm.setIdentityMap(identityMap);
+
+        SecurityDomain.Builder builder = SecurityDomain.builder()
+                .setDefaultRealmName("TestRealm");
+
+        builder.addRealm("TestRealm", simpleRealm).build();
+        builder.setRoleMapper(RoleMapper.constant(Roles.of("Test")));
+
+        builder.setPermissionMapper((principal, roles) -> PermissionVerifier.from(new LoginPermission()));
+
+        return builder.build();
+    }
+
+}
---
+++ undertow-standalone-jaspi/src/main/java/org/wildfly/security/examples/jaspi/SimpleServerAuthModule.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2018 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wildfly.security.examples.jaspi;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.security.auth.Subject;
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import javax.security.auth.message.AuthException;
+import javax.security.auth.message.AuthStatus;
+import javax.security.auth.message.MessageInfo;
+import javax.security.auth.message.MessagePolicy;
+import javax.security.auth.message.callback.GroupPrincipalCallback;
+import javax.security.auth.message.callback.PasswordValidationCallback;
+import javax.security.auth.message.module.ServerAuthModule;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * A simple {@link ServerAuthModule} implementation.
+ *
+ * @author <a href="mailto:darran.lofthouse@jboss.com">Darran Lofthouse</a>
+ */
+public class SimpleServerAuthModule implements ServerAuthModule {
+
+    private static final String USERNAME_HEADER = "X-USERNAME";
+    private static final String PASSWORD_HEADER = "X-PASSWORD";
+    private static final String MESSAGE_HEADER = "X-MESSAGE";
+
+    private CallbackHandler callbackHandler;
+
+    public void initialize(MessagePolicy requestPolicy, MessagePolicy responsePolicy, CallbackHandler handler, Map options)
+            throws AuthException {
+        System.out.println("SimpleServerAuthModule initialise");
+        this.callbackHandler = handle("handler", handler);
+    }
+
+    public Class[] getSupportedMessageTypes() {
+        System.out.println("SimpleServerAuthModule getSupportedMessageTypes");
+        return new Class[] { HttpServletRequest.class, HttpServletResponse.class };
+    }
+
+    public AuthStatus validateRequest(MessageInfo messageInfo, Subject clientSubject, Subject serviceSubject) throws AuthException {
+        System.out.println("validateRequest");
+        handle("messageInfo", messageInfo);
+        handle("clientSubject", clientSubject);
+        handle("serviceSubject", serviceSubject);
+
+        HttpServletRequest request =  (HttpServletRequest) messageInfo.getRequestMessage();
+        HttpServletResponse response = (HttpServletResponse) messageInfo.getResponseMessage();
+
+        final String username = request.getHeader(USERNAME_HEADER);
+        final String password = request.getHeader(PASSWORD_HEADER);
+
+        if (username == null || username.length() == 0 || password == null || password.length() == 0) {
+            response.addHeader(MESSAGE_HEADER, "Please resubmit the request with a username specified using the X-USERNAME and a password specified using the X-PASSWORD header.");
+            response.setStatus(401);
+            return AuthStatus.SEND_FAILURE;
+        }
+
+        try {
+            PasswordValidationCallback pvc = new PasswordValidationCallback(clientSubject, username, password.toCharArray());
+            callbackHandler.handle(new Callback[] { pvc, new GroupPrincipalCallback(clientSubject, new String[] {"Users"})});
+            if (pvc.getResult()) {
+                return AuthStatus.SUCCESS;
+            }
+
+            response.addHeader(MESSAGE_HEADER, "Validation of the supplied username and password failed, GO AWAY!!");
+            response.setStatus(401);
+            return AuthStatus.SEND_FAILURE;
+        } catch (IOException | UnsupportedCallbackException e) {
+            e.printStackTrace();
+            return AuthStatus.SEND_FAILURE;
+        }
+    }
+
+    public void cleanSubject(MessageInfo messageInfo, Subject subject) throws AuthException {
+        System.out.println("cleanSubject");
+    }
+
+    public AuthStatus secureResponse(MessageInfo messageInfo, Subject serviceSubject) throws AuthException {
+        System.out.println("secureResponse");
+        return AuthStatus.SEND_SUCCESS;
+    }
+
+    private static <T> T handle(final String name, final T value) {
+        System.out.println(String.format("'%s' is %s", name, value == null ? "null" : "not null"));
+
+        return value;
+    }
+
+}

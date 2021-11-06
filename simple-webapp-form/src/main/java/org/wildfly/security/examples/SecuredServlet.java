5bcc5b286d9848049caa8b3b7e189a9058b215f9
Darran Lofthouse <darran.lofthouse@jboss.com>
2021-11-06 13:45:40 +0000

    Add a simple-webapp but secured using FORM authentication.
    We should consider if a profile could allow simple-webapp
    to switch between the modes instead.

---
+++ simple-webapp-form/src/main/java/org/wildfly/security/examples/SecuredServlet.java
@@ -0,0 +1,81 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2017 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wildfly.security.examples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.Principal;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.HttpMethodConstraint;
+import javax.servlet.annotation.ServletSecurity;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * A simple secured HTTP servlet.
+ *
+ * @author <a href="mailto:darran.lofthouse@jboss.com">Darran Lofthouse</a>
+ */
+@WebServlet("/secured")
+@ServletSecurity(httpMethodConstraints = { @HttpMethodConstraint(value = "GET", rolesAllowed = { "Users" }) })
+public class SecuredServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (PrintWriter writer = resp.getWriter()) {
+            writer.println("<html>");
+            writer.println("  <head><title>Secured Servlet</title></head>");
+            writer.println("  <body>");
+            writer.println("    <h1>Secured Servlet</h1>");
+            writer.println("    <p>");
+            writer.print(" Current Principal '");
+            Principal user = req.getUserPrincipal();
+            writer.print(user != null ? user.getName() : "NO AUTHENTICATED USER");
+            writer.print("'");
+            writer.println("    </p>");
+
+            // Display some information about the HTTP session use if it exists.
+            HttpSession session = req.getSession(false);
+            if (session == null) {
+                writer.println("    <p>No HTTP Session exists.</p>");
+            } else {
+                Integer counter = (Integer) session.getValue("AccessCounter");
+                if (counter == null) {
+                    counter = 1;
+                } else {
+                    counter = counter + 1;
+                }
+                session.putValue("AccessCounter", counter);
+                writer.println(String.format("    <p>Session %s accessed %d time(s).</p>", session.getId(), counter));
+            }
+
+            // Display link to allow secured page to be revisited.
+            writer.println("    <p>");
+            writer.println("        <a href=\"./secured\">Access Secured Servlet</a>");
+            writer.println("    </p>");
+
+            writer.println("  </body>");
+            writer.println("</html>");
+        }
+    }
+
+}

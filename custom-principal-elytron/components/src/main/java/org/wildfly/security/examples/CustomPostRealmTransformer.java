7de8acd8af9eecbf0c1aa918b6ac7409f4aae6a4
Cameron Rodriguez <carodrig@redhat.com>
2023-01-27 13:27:51 -0500

    Add example for using a custom Principal with default Elytron authentication

---
+++ custom-principal-elytron/components/src/main/java/org/wildfly/security/examples/CustomPostRealmTransformer.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2023 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wildfly.security.examples;
+
+import java.security.Principal;
+
+import org.wildfly.extension.elytron.capabilities.PrincipalTransformer;
+
+/**
+ * A {@link PrincipalTransformer} to change the name of the {@link CustomPrincipal} after the realm is assigned.
+ *
+ * @author <a href="mailto:carodrig@redhat.com">Cameron Rodriguez</a>
+ */
+public class CustomPostRealmTransformer implements PrincipalTransformer {
+
+    private static final PrincipalTransformer delegate = CustomNameRewriter.asPrincipalTransformer(
+            "QuickstartUserPre", "QuickstartUserPost", false);
+
+    @Override
+    public Principal apply(Principal principal) {
+        return delegate.apply(principal);
+    }
+}

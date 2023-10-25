025d53c663e6e505a9292a0a4b4886fc0c8fe7c4
Ger-Jan te Dorsthorst <gerjantd@gmail.com>
2023-10-25 17:45:49 +0200

    Add https://github.com/Skyllarr/elytron-examples/commit/f7022b4e09dead731a2bb28f009d78fd4ba0a2e1; mvn clean test -Dtest=Client.java fails

--- resteasy-client-integration/client/src/test/java/Client.java
+++ resteasy-client-integration/client/src/test/java/Client.java
@@ -16,9 +16,11 @@ public class Client {
 
     @Test
     public void test() {
-        ((ResteasyClientBuilder)ClientBuilder.newBuilder()).hostnameVerifier(NoopHostnameVerifier.INSTANCE);
-        ResteasyClient sslClient = (ResteasyClient) ResteasyClientBuilder.newClient();
-        Response response = sslClient.target("https://127.0.0.1:8443/resteasy-client-integration-example/rest/hello").request().get();
+        Response response;
+        // Wrap AutoCloseable: https://www.oracle.com/technical-resources/articles/java/trywithresources.html
+        try (ResteasyClient sslClient = ((ResteasyClientBuilder) ClientBuilder.newBuilder()).hostnameVerifier(NoopHostnameVerifier.INSTANCE).build()) {
+            response = sslClient.target("https://127.0.0.1:8443/resteasy-client-integration-example/rest/hello").request().get();
+        }
         Assert.assertNotNull(response);
         Assert.assertEquals(response.getStatus(), 200);
         Assert.assertEquals(response.readEntity(String.class), "Hello jane");

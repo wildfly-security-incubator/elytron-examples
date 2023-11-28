4f53c33f547ebdfc961806a2a290270e863e1634
Diana Krepinska <skyllarr@gmail.com>
2023-11-28 20:32:50 +0100

    Migrate the jaas-realm example to Jakarta EE

--- jaas-realm/server-rest-endpoint/src/main/java/manual/test/JAXRSConfiguration.java
+++ jaas-realm/server-rest-endpoint/src/main/java/manual/test/JAXRSConfiguration.java
@@ -15,8 +15,8 @@
  */
 package manual.test;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
 
 /**
  * Configures a JAX-RS endpoint.
